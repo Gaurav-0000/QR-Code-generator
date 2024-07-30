36065e091a215f3b2f1640daaeffe6bbe3922ee4
Gaurav-0000 <166122683+Gaurav-0000@users.noreply.github.com>
2024-07-30 21:28:10 +0530

    qr.py

--- qr.py
+++ qr.py
@@ -3,7 +3,7 @@ qr = qrcode.QRCode(version=1,
 error_correction=qrcode.constants.ERROR_CORRECT_H,
 box_size=20,
 border=2)
-qr.add_data("https://in.pinterest.com")
+qr.add_data("https://www.wikipedia.org/")
 qr.make(fit='True')
 img = qr.make_image(fill_color="black" , back_color="white")
 img.save("qr.png")
