38dfe3fb7a26bf8671c33b4c603b4c245ff98bd5
Gaurav-0000 <166122683+Gaurav-0000@users.noreply.github.com>
2024-07-30 21:26:34 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,9 @@
+import qrcode
+qr = qrcode.QRCode(version=1,
+error_correction=qrcode.constants.ERROR_CORRECT_H,
+box_size=20,
+border=2)
+qr.add_data("https://in.pinterest.com")
+qr.make(fit='True')
+img = qr.make_image(fill_color="black" , back_color="white")
+img.save("qr.png")
